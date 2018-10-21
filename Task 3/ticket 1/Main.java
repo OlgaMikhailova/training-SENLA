f7dd836945affbc248d0280b61eae2dbd05c7d26
OlgaMikhailova <olmikhailava@gmail.com>
2018-10-21 18:11:48 +0300

    corrected the spaces and parentheses

--- Task 3/ticket 1/Main.java
+++ Task 3/ticket 1/Main.java
@@ -2,12 +2,10 @@ package com.company;
 
 public class Main
 {
-
-    public static void main(String[] args)
-    {
-      String text="Dot saw I was Tod";
+    public static void main(String[] args) {
+      String text = "Dot saw I was Tod";
       String[] s = text.split(" ");
-      for(int i=0; i<s.length;i++){
-      System.out.println(s[i]);}
+      for (int i = 0; i < s.length; i++){
+          System.out.println(s[i]);}
     }
 }
